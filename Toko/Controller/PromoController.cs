68261477584d3da137ee4afaec11ceb2e5d2ff20
rafifauzan09 <61864192+rafifauzan09@users.noreply.github.com>
2021-01-12 02:02:40 +0700

    Add files via upload

---
+++ Toko/Controller/PromoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Toko.Model;
+
+namespace Toko.Controller
+{
+    class PromoController
+    {
+        public List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
